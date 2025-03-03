e54318508c40c068571f51f99c6357a4d7ebad50
Sultan A. Dev <sultanabaniks@gmail.com>
2025-03-03 16:27:04 +0100

    initialize Next.js project with Tailwind CSS and basic layout components

---
+++ frontend/components/layout/SubscribeBanner.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const SubscribeBanner = () => {
+    return (
+        <Link href="/subscribe" className="block">
+          Hello
+        </Link>
+    );
+};
+
+export default SubscribeBanner;
---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        deepblack: "#141414", // Deep black
+        black: "#1A1A1A", // Dark gray
+        yellow: "#FFD11A",
+        lightgrey: "#666666",
+        white: "#FFFFFF",
+        grey60: "#98989A",
+        grey50: "#7E7E81",
+        dark20: "#333333"
+      },
+      screens: {
+        "max-500": { max: "500px" },
+        "max-400" : { max: "400px" },
+        "max-700" : { max: "700px" },
+        "max-900" : { max: "900px" }
+      }
+    },
+  },
+  plugins: [],
+} satisfies Config;
