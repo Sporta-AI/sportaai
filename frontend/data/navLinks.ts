704635537d1713abd489ea1e7a03d6581902c88f
Sultan A. Dev <sultanabaniks@gmail.com>
2025-03-04 06:05:36 +0100

    refactor: restructure project by removing unused files and adding Vite configuration

--- frontend/data/navLinks.ts
+++
@@ -1,6 +0,0 @@
-// export const navLinks = [
-//     { name: "Home", href: "/" },
-//     { name: "News", href: "/news" },
-//     { name: "Podcasts", href: "/podcasts" },
-//     { name: "Resources", href: "/resources" },
-// ];
